8dbb8e26fa878b6f707599a060bce67d79828869
CRdavey93 <39842745+CRdavey93@users.noreply.github.com>
2020-05-28 22:05:33 -0700

    Setup initial board and get user input.

--- GameOfLife/GameBoard.cs
+++ GameOfLife/GameBoard.cs
@@ -6,18 +6,58 @@ namespace GameOfLife
 {
     public class GameBoard
     {
-        public void CreateBoard(int width, int height)
+        //Size of the game
+        const int _width = 52;
+        const int _height = 52;
+
+        //A 2D array used to manipulate the visual board
+        private static Organism[,] boardState = new Organism[_width, _height];
+
+        public Organism[,] BoardState
         {
-            for (int y = 0; y < height; y++)
+            get => boardState;
+        }
+
+        //Create the game board and populate it with our starting Organisms
+        public void CreateBoard()
+        {
+            for (int y = 0; y < _height; y++)
             {
-                for (int x = 0; x < width; x++)
+                for (int x = 0; x < _width; x++)
                 {
-                    Console.Write((InsideOfRectangle(x, y, width, height)) ? " " : "*");
+                    if (InsideOfRectangle(x, y, _width, _height))
+                    {
+                        Console.Write(" ");
+                    }
+                    else
+                    {
+                        Console.Write("*");
+                    }
                 }
                 Console.WriteLine();
             }
         }
 
+        public void DrawOrganisms()
+        {
+            for (int y = 1; y < _height-1; y++)
+            {
+                for (int x = 1; x < _width-1; x++)
+                {
+                    Console.SetCursorPosition(x, y);
+                    if (HasLiveOrganism(x, y))
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+            }
+        }
+
+        //Helper method to check if our current location is inside the game board
         private bool InsideOfRectangle(int x, int y, int width, int height)
         {
             var topAndLeft = y > 0 && x > 0;
@@ -27,5 +67,30 @@ namespace GameOfLife
             return inBoundaries;
         }
 
+        //Helper method to check if the given location has an organism or not
+        private bool HasLiveOrganism(int x, int y)
+        {
+            if (boardState[x, y] == Organism.Live)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void SetupBoardArray()
+        {
+            var inputCoords = GameOfLife.InputCoords;
+            int firstCoord = 0;
+            int secondCoord = 0;
+
+            for (int i = 0; i < inputCoords.Count; i++)
+            {
+                var results = inputCoords[i].Split(',');
+                Int32.TryParse(results[0], out firstCoord);
+                Int32.TryParse(results[1], out secondCoord);
+                boardState[firstCoord, secondCoord] = Organism.Live;
+
+            }
+        }
     }
 }
--- GameOfLife/GameOfLife.cs
+++ GameOfLife/GameOfLife.cs
@@ -1,22 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace GameOfLife
 {
     public class GameOfLife
     {
-        enum Organism { live, dead, gestating, dying};
 
-        public void Play()
+        private static List<string> inputCoords = new List<string>();
+        private static int numOfGenerations;
+        private GameBoard newBoard;
+
+        public static List<string> InputCoords
+        {
+            get => inputCoords;
+        }
+
+        public void Run()
         {
             GetUserInput();
+            newBoard = new GameBoard();
+            newBoard.SetupBoardArray();
+            PlayGame();
         }
+        
+        private void PlayGame()
+        {
+            Console.Clear();
+            Console.CursorVisible = false;
+            newBoard.CreateBoard();
+            do
+            {
+                newBoard.DrawOrganisms();
+                SetupNextGen();
+                numOfGenerations--;
+            } while (numOfGenerations > 0);
+        }
+
         private static void GetUserInput()
         {
-            Console.Write("Please input coordinates for where you would like your organisms to start: ");
-            var a = Console.ReadLine();
-            Console.WriteLine(a);
+            Console.Write("Input coordinates for where you would like your organisms to start - format (x,x): ");
+            var userSetCoords = Console.ReadLine();
+            SetStartingCoords(userSetCoords);
+            Console.WriteLine(inputCoords[1]);
+            Console.Write("How many generations would you like the game to play?:");
+            numOfGenerations = Convert.ToInt32(Console.ReadLine());
+        }
+
+        private void SetupNextGen()
+        {
+            var currentBoard = newBoard.BoardState;
+
+        }
+
+        private static void SetStartingCoords(string userInput)
+        {
+            inputCoords = userInput.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            for (int i = 0; i < inputCoords.Count; i++)
+            {
+                inputCoords[i] = inputCoords[i].Replace("(", string.Empty).Replace(")", string.Empty);
+                inputCoords[i].Trim();
+            }
         }
     }
 }
