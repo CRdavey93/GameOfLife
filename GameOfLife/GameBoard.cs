a00a0ea322e4e21ca07d43961b70f5a9a87806c2
CRdavey93 <39842745+CRdavey93@users.noreply.github.com>
2020-06-03 12:59:41 -0700

    Core gameplay implemented. User input checks could be implemented and efficiency could likely be improved, but probably moving on for now.

--- GameOfLife/GameBoard.cs
+++ GameOfLife/GameBoard.cs
@@ -10,6 +10,32 @@ namespace GameOfLife
         const int _width = 52;
         const int _height = 52;
 
+        //Determine the borders of the game
+        const int _min_x = 1;
+        const int _min_y = 1;
+        const int _max_x = _width - 2;
+        const int _max_y = _height - 2;
+
+        public int MIN_X
+        {
+            get => _min_x;
+        }
+
+        public int MIN_Y
+        {
+            get => _min_y;
+        }
+
+        public int MAX_X
+        {
+            get => _max_x;
+        }
+
+        public int MAX_Y
+        {
+            get => _max_y;
+        }
+
         //A 2D array used to manipulate the visual board
         private static Organism[,] boardState = new Organism[_width, _height];
 
@@ -18,14 +44,14 @@ namespace GameOfLife
             get => boardState;
         }
 
-        //Create the game board and populate it with our starting Organisms
+        //Create the game board
         public void CreateBoard()
         {
             for (int y = 0; y < _height; y++)
             {
                 for (int x = 0; x < _width; x++)
                 {
-                    if (InsideOfRectangle(x, y, _width, _height))
+                    if (InsideOfRectangle(x, y))
                     {
                         Console.Write(" ");
                     }
@@ -38,6 +64,9 @@ namespace GameOfLife
             }
         }
 
+        /*
+         * Draw the Organisms to the board.
+         */
         public void DrawOrganisms()
         {
             for (int y = 1; y < _height-1; y++)
@@ -57,17 +86,23 @@ namespace GameOfLife
             }
         }
 
-        //Helper method to check if our current location is inside the game board
-        private bool InsideOfRectangle(int x, int y, int width, int height)
+        /* Helper method to check if our current location is inside the game board
+         * @param x the current x coordinate
+         * @param y the current y coordinate
+         */
+        private bool InsideOfRectangle(int x, int y)
         {
             var topAndLeft = y > 0 && x > 0;
-            var bottomAndRight = y < height - 1 && x < width - 1;
+            var bottomAndRight = y < _height - 1 && x < _width - 1;
             var inBoundaries = topAndLeft && bottomAndRight;
 
             return inBoundaries;
         }
 
-        //Helper method to check if the given location has an organism or not
+        /*Helper method to check if the given location has an organism or not
+         * @param x the current x coordinate
+         * @param y the current y coordinate
+         */
         private bool HasLiveOrganism(int x, int y)
         {
             if (boardState[x, y] == Organism.Live)
@@ -77,6 +112,9 @@ namespace GameOfLife
             return false;
         }
 
+        /*
+         * Method used to setup the initial board array given the input coordinates.
+         */
         public void SetupBoardArray()
         {
             var inputCoords = GameOfLife.InputCoords;
--- GameOfLife/GameOfLife.cs
+++ GameOfLife/GameOfLife.cs
@@ -18,14 +18,21 @@ namespace GameOfLife
             get => inputCoords;
         }
 
+        /*
+         * Runs the game when called
+         */                                                                         
         public void Run()
         {
             GetUserInput();
             newBoard = new GameBoard();
             newBoard.SetupBoardArray();
             PlayGame();
+            Console.WriteLine();
         }
         
+        /*
+         * Plays the game until the number of given generations has been played through
+         */
         private void PlayGame()
         {
             Console.Clear();
@@ -33,28 +40,144 @@ namespace GameOfLife
             newBoard.CreateBoard();
             do
             {
-                newBoard.DrawOrganisms();
+               newBoard.DrawOrganisms();
                 SetupNextGen();
                 numOfGenerations--;
             } while (numOfGenerations > 0);
         }
 
+        /*
+         * Gets the starting coordinates and number of desired generations from the player
+         */
         private static void GetUserInput()
         {
-            Console.Write("Input coordinates for where you would like your organisms to start - format (x,x): ");
+            Console.Write("Input coordinates for where you would like your organisms to start - format (x,y): ");
             var userSetCoords = Console.ReadLine();
             SetStartingCoords(userSetCoords);
-            Console.WriteLine(inputCoords[1]);
             Console.Write("How many generations would you like the game to play?:");
             numOfGenerations = Convert.ToInt32(Console.ReadLine());
         }
 
+        /*
+         * Sets up the board array for the next generation by iterating through the array and checking the neighbors organism status.
+         * The boards organisms are first set to a temporary status so each organism for the next gen can be calculated before changes are actually made.
+         * We then iterate through the board again, and set all of the temporary organisms to their final status for that generation.
+         */
         private void SetupNextGen()
         {
-            var currentBoard = newBoard.BoardState;
+            int count;
+            Organism currOrganism;
+            var currBoard = newBoard.BoardState;
+            for (int y = 1; y < currBoard.GetLength(1) - 1; y++)
+            {
+                for (int x = 1; x < currBoard.GetLength(0) - 1; x++)
+                {
+                    currOrganism = currBoard[x, y];
+                    count = 0;
+                    count = CheckNeighbors(x, y, currBoard);
+
+                    currBoard[x, y] = SetTempOrganismStatus(count, currOrganism);
+                }
+            }
+            for (int y = 1; y < currBoard.GetLength(1) - 1; y++)
+            {
+                for (int x = 1; x < currBoard.GetLength(0) - 1; x++)
+                {
+                    currOrganism = currBoard[x, y];
+
+                    currBoard[x, y] = SetFinalOrganismStatus(currOrganism);
+                }
+            }
 
         }
 
+        /* Helper method to check the surrounding organisms in the board array and determine the number of living neighbors
+         * @param currXPos is the current x coordinate that we are checking
+         * @param currYPos is the current y coordinate that we are checking
+         * @param currBoard is the array that we working on
+         */
+        private int CheckNeighbors(int currXPos, int currYPos, Organism[,]currBoard)
+        {
+            int count = 0;
+            int startPosX = (currXPos - 1 < newBoard.MIN_X) ? currXPos : currXPos - 1;
+            int startPosY = (currYPos - 1 < newBoard.MIN_Y) ? currYPos : currYPos - 1;
+            int endPosX = (currXPos + 1 > newBoard.MAX_X) ? currXPos : currXPos + 1;
+            int endPosY = (currYPos + 1 > newBoard.MAX_Y) ? currYPos : currYPos + 1;
+
+            for (int rowNum = startPosX; rowNum <= endPosX; rowNum++)
+            {
+                for (int colNum = startPosY; colNum <= endPosY; colNum++)
+                {
+                    if (rowNum == currXPos && colNum == currYPos)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        if (currBoard[rowNum, colNum] == Organism.Live || currBoard[rowNum, colNum] == Organism.Dying)
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+
+            return count;
+        }
+
+        /*Helper method to set the Organisms to their temporary status based on its current status and the status of its neighbors.
+         * We use a temporary status so that the next organism calculates its next state based on the current gen status of each neighbor
+         * @param count is the number of living neighbors that the current organism has
+         * @param currOrganism is the current status of the organism that we are checking
+         */
+        private Organism SetTempOrganismStatus(int count, Organism currOrganism)
+        {
+                    if (currOrganism == Organism.Live)
+                    {
+                        if (count < 2)
+                        {
+                            currOrganism = Organism.Dying;
+                        }
+                        else if (count > 3)
+                        {
+                            currOrganism = Organism.Dying;
+                        }
+                        else
+                        {
+                            return currOrganism;
+                        }
+                    }
+                    else if (currOrganism == Organism.Dead)
+                    {
+                        if (count == 3)
+                        {
+                            currOrganism = Organism.Gestating;
+                        }
+                    }
+
+            return currOrganism;
+        }
+
+        /*Helper method to set the final status of each organism from their temporary status to their final status
+         * @param currOrganism is the current status of the organism we are checking
+         */
+        private Organism SetFinalOrganismStatus(Organism currOrganism)
+        {
+            if (currOrganism == Organism.Gestating)
+            {
+                currOrganism = Organism.Live;
+            }
+            else if (currOrganism == Organism.Dying)
+            {
+                currOrganism = Organism.Dead;
+            }
+
+            return currOrganism;
+        }
+
+        /* Helper method to take the user input coordinates and turn them into a more useable format.
+         * 
+         */
         private static void SetStartingCoords(string userInput)
         {
             inputCoords = userInput.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
